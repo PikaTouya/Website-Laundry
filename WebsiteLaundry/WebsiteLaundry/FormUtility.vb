e7d4ae9b7a88a0b5d4d7b72df1861ec22d013369
PikaTouya <154250401+PikaTouya@users.noreply.github.com>
2023-12-29 00:26:17 +0700

    Add files via upload

---
+++ WebsiteLaundry/WebsiteLaundry/FormUtility.vb
@@ -0,0 +1,3 @@
+﻿Public Class FormUtility
+
+End Class
---
+++ WebsiteLaundry/WebsiteLaundry/MasterFormJasa.vb
@@ -0,0 +1,6 @@
+﻿Public Class MasterFormJasa
+    Private Sub IconBack_Click(sender As Object, e As EventArgs) Handles IconBack.Click
+        Me.Close()
+        Call FormUtama.OpenChildForm(New FormMaster)
+    End Sub
+End Class
